0b6b837a125cc7e9b30aa952941f474eee50a358
Ganesh <ganesh@ubuntu.ubuntu-domain>
2015-08-19 22:45:28 +0530

    search by zip/city

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -4,8 +4,11 @@ class StatesController < ApplicationController
   	@state = State.friendly.find(params[:id])
   	unless request.xhr?
   		state_ids = Agency.select("distinct state_id")
-  	@states = State.where(id: state_ids).order(:name) 
-  end
+  		@states = State.where(id: state_ids).order(:name) 
+  		filtered_infos = Agency.select("zip_code, city").where("state_id = ?", @state.id)
+  		@zip_codes = filtered_infos.collect(&:zip_code)
+  		@cities = filtered_infos.collect(&:city)
+  	end
 	@agencies = Agency.where("state_id = ?", @state.id).page(params[:page]).per(10).order(:organisation_name)
   end
 end
