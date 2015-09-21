27328229aa3a48bdaa575a6ed29289c9c684821e
Ganesh <ganesh@ubuntu.ubuntu-domain>
2015-09-21 21:52:50 +0530

    bug fix.

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,6 +1,6 @@
 class Website < ActiveRecord::Base
 	validates :domain, uniqueness: true	
-	validates :name, :domain, :city, :language, :state_id, presence: true
+	validates :name, :domain, :city, :state_id, presence: true
 
 	belongs_to :state
 end
